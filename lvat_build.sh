e67f79a8da8f441c3af66d05af8eb52edc0ca10b
Cel Skeggs <celskeggs@users.noreply.github.com>
2026-02-14 18:52:49 -0800

    Build and publish complete Docker container (#1)

--- lvat_build.sh
+++ lvat_build.sh
@@ -7,5 +7,17 @@ git config --global --add safe.directory "${PWD}/picolibc"
 mkdir build/
 cd build/
 cmake -G Ninja -DFETCHCONTENT_SOURCE_DIR_PICOLIBC=../picolibc ../arm-software/embedded/
+
+if [ "${1:-}" = "--dry-run" ]
+then
+    for arch in x86_64 AArch64
+    do
+        mkdir LVAT-Linux-${arch}
+        mkdir LVAT-Linux-${arch}/bin LVAT-Linux-${arch}/include LVAT-Linux-${arch}/lib
+        tar -cJf LVAT-Linux-${arch}.tar.xz LVAT-Linux-${arch}
+    done
+    exit
+fi
+
 ninja llvm-toolchain
 ninja package-llvm-toolchain
