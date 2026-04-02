17959017e64a4005b65c1d266e48aaa3c9314546
Andrei Tumbar <andrei.tumbar@jpl.nasa.gov>
2026-04-02 18:22:28 +0000

    Add to readme

--- compiler-rt/lib/builtins/arm/badstrb_strb.c
+++ compiler-rt/lib/builtins/arm/badstrb_strb.c
@@ -15,8 +15,10 @@ __badstrb_strb(uint8_t *address, uint8_t value) {
 
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
   const uint16_t shift = (address_i & 0x1) * 8;
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+  const uint16_t shift = (!(address_i & 0x1)) * 8;
 #else
-  uint16_t shift = (!(address_i & 0x1)) * 8;
+  #error "badstrb is only supported on little endian"
 #endif
 
   const uint16_t value_mask = (uint16_t)value << shift;
